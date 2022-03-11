fb6ed205474b93dc3b4f1ddc728bd06b62c51192
yonatanski <phpcodeyg@gmail.com>
2022-03-11 16:58:37 +0100

    background and styling modifeied

--- weather_app-typescript/src/Components/Home.tsx
+++ weather_app-typescript/src/Components/Home.tsx
@@ -6,13 +6,11 @@ function WeatherInput() {
   const [warsaw, setWarsaw] = useState<ICity>()
 
   const [london, setLondon] = useState<ICity>()
-  const [newYork, setNewYork] = useState<ICity>()
-  const [tokyo, setTokyo] = useState<ICity>()
+
   const [searchedCity, setSearchedCity] = useState<ICity>()
   const [query, setQuery] = useState("")
 
   const getWarsawCity = async () => {
-    const key = process.env.REACT_APP_PRIVATE_KEY
     try {
       const res = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=Torun&units=metric&appid=3a1bb83a47a144256eba69e7dd57dc4c")
       if (res.ok) {
@@ -27,7 +25,6 @@ function WeatherInput() {
     }
   }
   const getLondonCity = async () => {
-    const key = process.env.REACT_APP_PRIVATE_KEY
     try {
       const res = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=3a1bb83a47a144256eba69e7dd57dc4c")
       if (res.ok) {
@@ -83,36 +80,35 @@ function WeatherInput() {
               <Card.Header>{searchedCity.city.name}</Card.Header>
               <Card.Body>
                 <Card.Title>{searchedCity.list[1].weather[0].description} </Card.Title>
-                <Card.Title>{searchedCity.list[0].main.temp} </Card.Title>
-                <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                <Card.Title>{searchedCity.list[0].main.temp}C° </Card.Title>
               </Card.Body>
             </Card>
           ) : (
             <>
               <Col xs="12" md="6" style={{ marginTop: "3.5rem" }}>
                 <Col xs="12">
-                  <Card style={{ backgroundColor: "black", marginBottom: "2rem", borderRadius: "10%", width: "75%" }}>
+                  <Card style={{ backgroundColor: "lightblue", marginBottom: "2rem", borderRadius: "10%", width: "75%" }}>
                     <Card.Img src={`http://openweathermap.org/img/wn/${warsaw?.list[0].weather[0].icon}@2x.png`} />
                     <Card.ImgOverlay>
-                      <Card.Title style={{ fontSize: "1.8rem", fontWeight: "bold" }}>{warsaw?.city.name}</Card.Title>
+                      <Card.Title style={{ fontSize: "1.8rem", fontWeight: "bold", color: "black" }}>{warsaw?.city.name}</Card.Title>
                       <Card.Text>
-                        {warsaw?.list[0].weather[0].main} <span style={{ color: "blue" }}> {warsaw?.list[0].main.temp}C° </span>
+                        {warsaw?.list[0].weather[0].main} <span style={{ color: "yellow" }}> {warsaw?.list[0].main.temp}C° </span>
                       </Card.Text>
-                      <Card.Text style={{ fontSize: "1.2rem", fontStyle: "italic", color: "blue", backgroundColor: "white" }}>
+                      <Card.Text style={{ fontSize: "1.2rem", fontStyle: "italic", color: "white", backgroundColor: "black" }}>
                         "{warsaw?.list[0].weather[0].description}"{/* <img src={`http://openweathermap.org/img/wn/${milano?.list[0].weather[0].icon}@2x.png`} alt="" /> */}
                       </Card.Text>
                     </Card.ImgOverlay>
                   </Card>
                 </Col>
                 <Col xs="12">
-                  <Card style={{ backgroundColor: "black", marginBottom: "2rem", borderRadius: "10%", width: "75%" }}>
+                  <Card style={{ backgroundColor: "lightblue", marginBottom: "2rem", borderRadius: "10%", width: "75%" }}>
                     <Card.Img src={`http://openweathermap.org/img/wn/${london?.list[0].weather[0].icon}@2x.png`} />
                     <Card.ImgOverlay>
-                      <Card.Title style={{ fontSize: "1.8rem", fontWeight: "bold" }}>{london?.city.name}</Card.Title>
+                      <Card.Title style={{ fontSize: "1.8rem", fontWeight: "bold", color: "black" }}>{london?.city.name}</Card.Title>
                       <Card.Text>
-                        {london?.list[0].weather[0].main} <span style={{ color: "blue" }}> {london?.list[0].main.temp}C° </span>
+                        {london?.list[0].weather[0].main} <span style={{ color: "yellow" }}> {london?.list[0].main.temp}C° </span>
                       </Card.Text>
-                      <Card.Text style={{ fontSize: "1.2rem", fontStyle: "italic", color: "blue", backgroundColor: "white" }}>
+                      <Card.Text style={{ fontSize: "1.2rem", fontStyle: "italic", color: "white", backgroundColor: "black" }}>
                         "{london?.list[0].weather[0].description}"{/* <img src={`http://openweathermap.org/img/wn/${milano?.list[0].weather[0].icon}@2x.png`} alt="" /> */}
                       </Card.Text>
                     </Card.ImgOverlay>
